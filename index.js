59a0cbb65ba2c0d5c9d11acd26e67ec977971e6d
Luke Bonaccorsi <luke@lukeb.co.uk>
2014-12-05 02:32:19 +0000

    Added exclude list

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ const issue = client.issue('iojs/io.js', 37)
 const http = require('http')
 const ecstatic = require('ecstatic')({ root: __dirname + '/public' })
 const isImage = /(!\[.*?\]\()(.+?)(\))/g
+const excludedImages = [
+  'https://cloud.githubusercontent.com/assets/4780756/5292274/8891edee-7b57-11e4-8358-3d27dbf057f2.jpg',
+  'https://cloud.githubusercontent.com/assets/254782/5296489/b8144ea2-7ba4-11e4-811b-bb7a45319497.jpg',
+  'https://camo.githubusercontent.com/5a195429051ea35ea439d22c9e777ebfcb49a274/68747470733a2f2f692e696d6775722e636f6d2f69344e594668412e676966',
+  'https://camo.githubusercontent.com/f7af2c468a8175239dd5cd1c6eef2ff1b6db1e25/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f662e636c2e6c792f6974656d732f3276304e336330413350337333513378334433302f6e6f742d676f6f676c652d696f2e6a7067',
+  'https://cloud.githubusercontent.com/assets/58871/5309797/7a49a626-7c21-11e4-8d1b-a44ebfc45da5.png'
+];
 
 var logos = [{
   url: 'https://camo.githubusercontent.com/f64eb2e8e06a5b3921076a117d9084811c7ffca8/687474703a2f2f736372617463682e737562737461636b2e6e65742f696f6a732e7376673f31',
@@ -103,7 +110,9 @@ function pullImageUrlsFromMarkdown (md) {
   var urls = []
   var match = isImage.exec(md)
   while (match !== null) {
-    urls.push(match[2])
+    if (excludedImages.indexOf(match[2]) === -1 ) {
+      urls.push(match[2])
+    }
     match = isImage.exec(md)
   }
   return urls
