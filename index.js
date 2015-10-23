06d3102107bac2bec78b476ee4c1f19127e89936
Oli Evans <oli@zilla.org.uk>
2015-10-23 10:27:33 +0100

    Flip the switches for nodejs fun

--- index.js
+++ index.js
@@ -9,34 +9,11 @@ winston.add(winston.transports.Console, { timestamp: true })
 if (!process.env.GH_KEY) winston.warn('No GH_KEY found in ENV. You may be throttled')
 
 const client = github.client(process.env.GH_KEY)
-const issue = client.issue('iojs/io.js', 37)
+const issue = client.issue('nodejs/evangelism', 179)
 const isImage = /(!\[.*?\]\()(.+?)(\))/g
-const excludedImages = [
-  'http://nodeforward.org/node-forward.png',
-  'http://www.crewpatches.com/images/patch_nasa_vector_ii_240x240.jpg',
-  'https://m1.behance.net/rendition/modules/14539849/disp/0a5204aa473b5d26fc0efed400fe83ce.jpg',
-  'https://m1.behance.net/rendition/modules/14539847/disp/7c8850a73c420ec3bc30d361e65a5751.jpg',
-  'http://www.galacticvoyager.com/collect/nasaworm330.jpg',
-  'https://cloud.githubusercontent.com/assets/4780756/5292274/8891edee-7b57-11e4-8358-3d27dbf057f2.jpg',
-  'https://cloud.githubusercontent.com/assets/254782/5296489/b8144ea2-7ba4-11e4-811b-bb7a45319497.jpg',
-  'https://i.imgur.com/i4NYFhA.gif',
-  'https://s3.amazonaws.com/f.cl.ly/items/2v0N3c0A3P3s3Q3x3D30/not-google-io.jpg',
-  'https://cloud.githubusercontent.com/assets/58871/5309797/7a49a626-7c21-11e4-8d1b-a44ebfc45da5.png',
-  'http://fisica.cab.cnea.gov.ar/estadistica/abramson/celestia/gallery/slides/Io-Jupiter.jpg'
-]
+const excludedImages = []
 
-var logos = [{
-  url: 'https://camo.githubusercontent.com/f64eb2e8e06a5b3921076a117d9084811c7ffca8/687474703a2f2f736372617463682e737562737461636b2e6e65742f696f6a732e7376673f31',
-  comment: {
-    url: 'https://github.com/iojs/io.js/issues/37',
-    body: 'Let\'s all make some logos!'
-  },
-  user: {
-    login: 'substack',
-    avatar_url: 'https://avatars1.githubusercontent.com/u/12631?v=3&s=400',
-    html_url: 'https://github.com/substack'
-  }
-}]
+var logos = []
 
 init()
 
@@ -51,7 +28,7 @@ function init () {
   function storeLogos (err, res) {
     if (err) return winston.error(err)
     // rebuild the list. Keep the first one, as it's not in the commments but the issue.
-    logos = [logos[0]].concat(res)
+    logos = res
     fs.writeFile(__dirname + '/public/logos.json', JSON.stringify(logos))
   }
 }
